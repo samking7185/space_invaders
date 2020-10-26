688093684966e2c24dd86973f0f3bd8e988fd1d8
samking7185 <47400683+samking7185@users.noreply.github.com>
2020-10-26 17:03:18 -0400

    Created Fuzzy class in fuzzy.py

---
+++ fuzzy.py
@@ -0,0 +1,92 @@
+import numpy as np
+import os
+import time
+import random
+import math
+
+class Membership:
+    def __init__(self, x):
+        self.x = x
+        self.le = None
+        self.ce = None
+        self.re = None
+        self.mu = None
+
+    def triangle(self):
+        x = self.x
+        le = self.le
+        ce = self.ce
+        re = self.re
+        if x >= le and x < ce:
+            mu = (x - le)/(ce - le)
+        elif x >= ce and x <= re:
+            mu = (re - x)/(re - ce)
+        else:
+            mu = 0
+        self.mu = mu
+
+    def rshlder(self):
+        x = self.x
+        le = self.le
+        ce = self.ce
+        re = self.re
+        if x >= le and x < ce:
+            mu = (x - le)/(ce - le)
+        elif x >= ce and x <= re:
+            mu = 1
+        else:
+            mu = 0
+        self.mu = mu
+
+    def lshlder(self):
+        x = self.x
+        le = self.le
+        ce = self.ce
+        re = self.re
+        if x >= le and x < ce:
+            mu = 1
+        elif x >= ce and x <= re:
+            mu = (re - x)/(re - ce)
+        else:
+            mu = 0
+        self.mu = mu
+
+    def output(self):
+        x = self.x
+        le = self.le
+        ce = self.ce
+        re = self.re
+        out = (le, ce, re)
+
+class Rulebase:
+    def __init__(self, input):
+        self.input = input
+
+    def AND_rule(self):
+        input = self.input
+        mu_out = max(input)
+
+    def OR_rule(self):
+        input = self.input
+        mu_out = min(input)
+
+class Defuzz:
+    def __init__(self, mu, output):
+        self.mu = mu
+        self.output = output
+        self.crisp = None
+
+    def defuzz_out(self):
+        A_out = []
+        A_outC = []
+        for i in self.mu:
+            # self.output should be list of tuples
+            # (le, ce, re)
+            outMF = list(self.output[i])
+            A_outN = 0.5*i*(outMF[0] - outMF[2])
+            A_out.append(A_outN)
+            A_outC.append(outMF[1])
+        union = np.sum(A_out)
+        out_Num = np.multiply(A_out, A_outC)
+        crisp_out = np.sum(out_Num)/union
+        self.crisp = crisp_out
